f22664c0c13ee81c5758649130790dcb40257a2d
duyleomessi <duyleitbka95@gmail.com>
2016-03-17 00:42:48 +0700

    http server version 2

---
+++ node-http/server-2.js
@@ -0,0 +1,47 @@
+/**
+ * Created by root on 16/03/2016.
+ */
+
+
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var server = http.createServer(function (req, res) {
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+
+    if (req.method === 'GET') {
+        var fileUrl;
+        if (req.url === '/') fileUrl = 'index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        var fileExt = path.extname(filePath);
+
+        if (fileExt === '.html') {
+            fs.exists(filePath, function (exists) {
+                if (!exists) {
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    res.end('<h1>Error 404: ' + fileUrl + ' not found</h1>');
+                    return;
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.writeHead(404, {'Type-Content': 'text/html'});
+            res.end('<h1>Error 404: ' + fileUrl + ' not a html file</h1>');
+        }
+
+    } else {
+        res.writeHead(404, {'Content-type': 'text/html'});
+        res.end('<h1>Error 404: ' + req.method + ' doesnot support</h1>');
+    }
+});
+
+server.listen(port, hostname, function () {
+    console.log('Server runing at http://' + hostname + ':' + port + '/');
+});
